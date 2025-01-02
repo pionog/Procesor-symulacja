eaf9cc5b2ea814ce4865dd16395098d7b4ea77fc
Piotr Noga <109363349+pionog@users.noreply.github.com>
2025-01-02 22:42:12 +0100

    Fixed buttons in main menu + testing dynamic high scores

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -4,7 +4,23 @@ using UnityEngine;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public void exitButton() {
+    public ScoreTableManager scoreTableManager;
+
+    public void ExitButton() {
         Application.Quit();
+    }
+    public void ScoreButton()
+    {
+        Debug.Log("Nacisnieto przycisk wynikow");
+        ScoreEntry[] scores = new ScoreEntry[]
+        {
+            new ScoreEntry(150, "Player1"),
+            new ScoreEntry(200, "Player2"),
+            new ScoreEntry(120, "Player3")
+        };
+
+        // Wczytaj dane do tabeli
+        
+        scoreTableManager.LoadScores(scores);
     }
 }
