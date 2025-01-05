ea22a112eed537e8f16e1504455c388f0158ca28
Piotr Noga <109363349+pionog@users.noreply.github.com>
2025-01-05 03:38:53 +0100

    Dodanie okna z dodawaniem mnemoników
    Dodano możliwość definiowania czynności danego mnemonika linijka po linijce. Na razie działa tylko ustawianie parametrów. Na tym etapie jeszcze nie funkcjonuje zapisywanie informacji wprowadzonych w tabelce.

--- Assets/Scripts/Microcode/MicrocodeEditor.cs
+++ Assets/Scripts/Microcode/MicrocodeEditor.cs
@@ -1,39 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class MicrocodeEditor : MonoBehaviour
 {
     public Transform tableParent; // Kontener dla wierszy
     public GameObject rowPrefab; // Prefab wiersza tabeli
-    private MicrocodeTable microcodeTable = new MicrocodeTable();
+    public MicrocodeTable microcodeTable;
+    public GameObject AddNewRowButton;
 
     public void AddRow()
     {
         int address = microcodeTable.GetAllRows().Count();
-        var newRow = new MicrocodeRow(address);
+        Debug.Log("Adres lokalny to: " + address);
+        var newRow = gameObject.AddComponent<MicrocodeRow>();
         microcodeTable.AddRow(newRow);
 
         var rowUI = Instantiate(rowPrefab, tableParent);
-        var inputs = rowUI.GetComponentsInChildren<InputField>();
+        RectTransform rowRectTransform = rowUI.GetComponent<RectTransform>();
+        rowRectTransform.sizeDelta = new Vector2(tableParent.GetComponent<RectTransform>().rect.width, rowRectTransform.sizeDelta.y);
 
-        // Powi¹¿ pola z danymi
-        inputs[0].text = address.ToString("X4"); // uAR
-        inputs[1].onEndEdit.AddListener(value => newRow.Label = value);
-        inputs[2].onEndEdit.AddListener(value => newRow.ALU = value);
-        inputs[3].onEndEdit.AddListener(value => newRow.S1 = value);
-        inputs[4].onEndEdit.AddListener(value => newRow.S2 = value);
-        inputs[5].onEndEdit.AddListener(value => newRow.Dest = value);
-        inputs[6].onEndEdit.AddListener(value => newRow.ExtIR = value);
-        inputs[7].onEndEdit.AddListener(value => newRow.Const = int.TryParse(value, out int result) ? result : (int?)null);
-        inputs[8].onEndEdit.AddListener(value => newRow.JCond = value);
-        inputs[9].onEndEdit.AddListener(value => newRow.Adr = value);
-        inputs[10].onEndEdit.AddListener(value => newRow.Mem = value);
-        inputs[11].onEndEdit.AddListener(value => newRow.MAdr = value);
-        inputs[12].onEndEdit.AddListener(value => newRow.MDest = value);
-        inputs[13].onEndEdit.AddListener(value => newRow.Regs = value);
+        AddNewRowButton.transform.SetAsLastSibling();
+
+
+        var Const = rowUI.GetComponentInChildren<TMP_InputField>();
+        var dropdowns = rowUI.GetComponentsInChildren<TMP_Dropdown>();
+
+        // Ustaw listenery dla Dropdownów
+        dropdowns[0].onValueChanged.AddListener(index => newRow.ALU = dropdowns[0].options[index].text);        //0
+        dropdowns[1].onValueChanged.AddListener(index => newRow.S1 = dropdowns[1].options[index].text);         //1
+        dropdowns[2].onValueChanged.AddListener(index => newRow.S2 = dropdowns[2].options[index].text);         //2
+        dropdowns[3].onValueChanged.AddListener(index => newRow.Dest = dropdowns[3].options[index].text);       //3
+                    Const.onEndEdit.AddListener(value => newRow.Const = int.Parse(Const.text));                 //4
+        dropdowns[4].onValueChanged.AddListener(index => newRow.JCond = dropdowns[4].options[index].text);      //5
+        dropdowns[5].onValueChanged.AddListener(index => newRow.Mem = dropdowns[5].options[index].text);        //6
+        dropdowns[6].onValueChanged.AddListener(index => newRow.MAdr = dropdowns[6].options[index].text);       //7
+        dropdowns[7].onValueChanged.AddListener(index => newRow.MDest = dropdowns[7].options[index].text);      //8
+        dropdowns[8].onValueChanged.AddListener(index => newRow.Regs = dropdowns[8].options[index].text);       //9
+
     }
 
     public void PrintTable()
